35b4aac06216e7362db54007d5cf9e2ad843a5cd
Paul Williamson <squarefrog@gmail.com>
2014-05-18 10:01:38 +0100

    Initial Commit

---
+++ TravisTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TravisTest
+//
+//  Created by Paul Williamson on 18/05/2014.
+//  Copyright (c) 2014 Paul Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravisTestTests/TravisTestTests.m
@@ -0,0 +1,34 @@
+//
+//  TravisTestTests.m
+//  TravisTestTests
+//
+//  Created by Paul Williamson on 18/05/2014.
+//  Copyright (c) 2014 Paul Williamson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TravisTestTests : XCTestCase
+
+@end
+
+@implementation TravisTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
